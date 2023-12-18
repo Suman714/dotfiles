ada9deac091efcfbe99082c8199fb52720496e4b
Suman <sumanhansda141@outlook.com>
2023-12-18 22:35:07 +0530

    focused bg to #000000.

--- awesome/mytheme.lua
+++ awesome/mytheme.lua
@@ -14,7 +14,7 @@ local theme = {}
 theme.font          = "Noto Sans Mono 8"
 
 theme.bg_normal     = "#222222"
-theme.bg_focus      = "#3e3e42"
+theme.bg_focus      = "#000000"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
